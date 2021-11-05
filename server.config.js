a14433fb520877b21722ca97139cc398d7e79071
Maksym Shamko Raptom <max.shamko25@gmail.com>
2021-11-05 07:59:26 +0000

    working

--- server.config.js
+++ server.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'Server.ts',
       interpreter: 'bash',
-      script: 'yarn',
+      script: 'yarn.sh',
       args: 'server',
       watch: false,
       autorestart: true,
